50386293f557733c1b90dc57316d6f06cc97c476
Juergen Neumann <juergen@junes.eu>
2016-03-18 20:27:32 +0100

    Several minar fixes, including start script.

--- bin/deepamehta.sh
+++ bin/deepamehta.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+
 cd "$(dirname "$0")"
+
 if [ -z $1 ]; then
     CONFIG="/etc/deepamehta/deepamehta.conf"
 else
     CONFIG="$1"
 fi
+
 if [ ! -f ${CONFIG} ]; then
     echo "ERROR! Config file ${CONFIG} not found."
     exit 1
 fi
 
-# Read configuration variables file if it is present
-[ -r /etc/default/${NAME} ] && . /etc/default/${NAME}
-
-exec java -Xms${DM_JAVA_XMS}M -Xmx${DM_JAVA_XMX}M -Dfile.encoding=UTF-8 -Dfelix.system.properties=file:${CONFIG} -jar bin/felix.jar
+if [ -r /etc/default/deepamehta ]; then 
+    . /etc/default/deepamehta
+    exec java -Xms${DM_JAVA_XMS}M -Xmx${DM_JAVA_XMX}M -Dfile.encoding=UTF-8 -Dfelix.system.properties=file:${CONFIG} -jar bin/felix.jar
+else
+    echo "WARNING! Config file /etc/default/deepamehta not found. Starting with default JAVA memory settings."
+    exec java -Dfile.encoding=UTF-8 -Dfelix.system.properties=file:${CONFIG} -jar bin/felix.jar
+fi
